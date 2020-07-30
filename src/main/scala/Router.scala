227150e1d18c9d3fb2b16dda6285376534ec3512
NPCRUS <npcrus@gmail.com>
2020-07-30 15:23:00 +0200

    cors what an actual fuck

--- src/main/scala/Router.scala
+++ src/main/scala/Router.scala
@@ -1,18 +1,21 @@
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Directive0, RejectionHandler, Route}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import routes.{BattleRoute, InventoryRoute, MeRoute, TestRoute}
 
 object Router {
 
-  def apply(): Route = cors() {
-    concat(
-      new TestRoute().getRoutes,
-      new MeRoute().getRoutes,
-      new BattleRoute().getRoutes,
-      new InventoryRoute().getRoutes
-    )
-  }
+  val handleErrors: Directive0 = handleRejections(corsRejectionHandler.withFallback(RejectionHandler.default))
+
+  def apply(): Route =
+    cors() {
+      handleErrors {
+        new TestRoute().getRoutes ~
+          new MeRoute().getRoutes ~
+          new BattleRoute().getRoutes ~
+          new InventoryRoute().getRoutes
+      }
+    }
 
 }
 
